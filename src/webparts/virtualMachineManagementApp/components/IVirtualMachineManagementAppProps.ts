7c5a249057d64b3a38900b16aaf53d8c8661ab13
Sebastian Nagy <sebastian.nagy@rhomberg.com>
2023-02-21 14:15:57 +0100

    remove - not working dir

--- src/webparts/virtualMachineManagementApp/components/IVirtualMachineManagementAppProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IVirtualMachineManagementAppProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
