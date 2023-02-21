51e54f0112071a0bda79eecee7ce914284dd8094
Sebastian Nagy <sebastian.nagy@rhomberg.com>
2023-02-21 14:41:37 +0100

    update - working init

---
+++ src/webparts/rhombergStudioSpfxTailwindApp/components/IRhombergStudioSpfxTailwindAppProps.ts
@@ -0,0 +1,7 @@
+export interface IRhombergStudioSpfxTailwindAppProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
